7c31fadb0b321319f612664c85389ef05ad6e938
RaviTh <ravi.thapa@csireg.com>
2018-09-13 07:45:41 -0500

    Hello world Print Program

---
+++ hello_world/src/com/cai/test/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.cai.test;
+
+
+/**
+ * 
+ * @author ravidream
+ *
+ */
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World !");
+	}
+	
+}
