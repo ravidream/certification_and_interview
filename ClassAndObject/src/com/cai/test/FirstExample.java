925fc243708689f68aea89c6e9539fefac506089
RaviTh <ravi.thapa@csireg.com>
2018-09-15 04:44:16 -0500

    Class and Object examples.

---
+++ ClassAndObject/src/com/cai/test/FirstExample.java
@@ -0,0 +1,16 @@
+package com.cai.test;
+
+public class FirstExample {
+	String str = "Hello World";
+	int i = 10;
+	float f = 10.5f;
+	
+	public static void main(String[] args) {
+		
+		FirstExample fe = new FirstExample();
+		System.out.println("str : "+fe.str);
+		System.out.println("i : "+fe.i);
+		System.out.println("f : "+fe.f);
+	}
+
+}
---
+++ ClassAndObject/src/com/cai/test/MyClass.java
@@ -0,0 +1,21 @@
+package com.cai.test;
+
+public class MyClass {
+   private static int count = 0;
+   private int x;
+
+   public MyClass(int i) {
+      x = i;
+   }
+   public void incrementCount() {
+      count++;      
+   }
+
+   public void printX()  {
+      System.out.println("Value of x : "  + x);
+   }
+
+   public static void printCount() {
+      System.out.println("Value of count : "  + count);
+   }
+}
